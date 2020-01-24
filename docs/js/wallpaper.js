c89bc9f3ac466cd4e37fdd171bb7317647667ce1
prikhodkin <info@prikhodkin.com>
2020-01-24 15:37:45 +0500

    remove arrows for hover and fix photo columns

--- docs/js/wallpaper.js
+++ docs/js/wallpaper.js
@@ -16,20 +16,20 @@ function showMenuItem(item, delay, view) {
 $(window).on('load', function () {
   setTimeout(function () {
     $('.wallpaper').fadeOut('slow');
-  }, 20000);
+  }, 7000);
   setTimeout(function () {
     window.location.replace('about.html');
-  }, 20500);
+  }, 7500);
   setTimeout(function () {
     $('.wallpaper__inner').addClass('wallpaper__logo-finish');
-  }, 6000);
+  }, 1000);
   setTimeout(function () {
     $('.wallpaper__hide').addClass('wallpaper__hide--show');
-  }, 7000);
+  }, 1500);
   setTimeout(function () {
     for (var i = 0; i <= menuItems.length; i++) {
       showMenuItem(menuItems[i], time, 'show');
       time += 500;
     }
-  }, 8000);
+  }, 2000);
 });
--- src/js/wallpaper.js
+++ src/js/wallpaper.js
@@ -15,26 +15,26 @@ $(window).on('load', () => {
 
   setTimeout(() => {
     $('.wallpaper').fadeOut('slow');
-  }, 20000);
+  }, 7000);
 
   setTimeout(() => {
     window.location.replace('about.html');
-  }, 20500);
+  }, 7500);
 
   setTimeout(() => {
     $('.wallpaper__inner').addClass('wallpaper__logo-finish');
 
-  }, 6000);
+  }, 1000);
 
   setTimeout(() => {
     $('.wallpaper__hide').addClass('wallpaper__hide--show');
-  }, 7000);
+  }, 1500);
 
   setTimeout(() => {
     for (let i = 0; i <= menuItems.length; i++) {
       showMenuItem(menuItems[ i ], time, 'show');
       time += 500;
     }
-  }, 8000);
+  }, 2000);
 
 });
